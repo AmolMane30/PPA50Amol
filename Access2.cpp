69435492cf7b5d7e71ed00d6df7e4e22942ea1e1
AmolMane30 <maneamol3001@gmail.com>
2024-05-10 20:57:26 +0530

    Add files via upload

---
+++ Access2.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int A;
+    private:
+        int B;
+    protected:
+        int C;
+    public:
+        Base()
+        {
+            A = 10; B = 20; C = 30;
+        }
+        void Fun()
+        {
+            cout<<A<<"\n";  // Allowed
+            cout<<B<<"\n";  // Allowed
+            cout<<C<<"\n";  // Allowed
+        }
+};
+class Derived : public Base
+{
+    public:
+        void Gun()
+        {
+            cout<<A<<"\n";  // Allowed
+            cout<<B<<"\n";  // Not allowed
+            cout<<C<<"\n";  // Allowed
+        }
+};
+
+int main()
+{
+    Base bobj;
+
+    cout<<bobj.A<<"\n"; // Allowed
+    cout<<bobj.B<<"\n"; // Not allowed
+    cout<<bobj.C<<"\n"; // Not allowed
+
+    return 0;
+}
